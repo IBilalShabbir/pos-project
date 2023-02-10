f6f704a3a7995c5e32943c8c42a749f74d2a9c02
Asim Rasheed <asimwarraich533@gmail.com>
2023-02-10 13:05:03 +0500

    fix files

--- src/screens/dashboard/notifications.jsx
+++ src/screens/dashboard/notifications.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function notifications() {
   return <div>notifications</div>;
 }
--- src/screens/dashboard/product.jsx
+++ src/screens/dashboard/product.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function product() {
   return <div>product</div>;
 }
