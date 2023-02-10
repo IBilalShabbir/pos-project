9c63ede588d0edef4883a3d0869a93b257986431
Asim Rasheed <asimwarraich533@gmail.com>
2023-02-10 13:30:40 +0500

    total statat card

--- src/screens/dashboard/index.jsx
+++ src/screens/dashboard/index.jsx
@@ -2,8 +2,32 @@ import React from "react";
 
 export default function index() {
   return (
-    <div className="home__container">
-      <div className="home__container__content"></div>
+    <>
+      <HomeStatCard
+        color="white"
+        heading="12300"
+        subheading="Total Sales(rs)"
+      />
+      <HomeStatCard
+        color="white"
+        heading="12300"
+        subheading="Total Sales(rs)"
+      />
+      <HomeStatCard
+        color="white"
+        heading="12300"
+        subheading="Total Sales(rs)"
+      />
+      Home
+    </>
+  );
+}
+
+function HomeStatCard({ color, heading, subheading }) {
+  return (
+    <div className="home__header__card" style={{ backgroundColor: color }}>
+      <div className="home__header__card__heading">{heading}</div>
+      <div className="home__header__card__subheading">{subheading}</div>
     </div>
   );
 }
