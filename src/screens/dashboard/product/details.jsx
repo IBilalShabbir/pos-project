ee93ba29984b1b89f874741a5c08f3177178d4e1
Arslan Iftikhar <a44222666@gmail.com>
2023-02-17 16:08:45 +0500

    product details base

---
+++ src/screens/dashboard/product/details.jsx
@@ -0,0 +1,49 @@
+import { ButtonPrimary, Header } from "../../../components";
+
+function Details() {
+  return (
+    <>
+      <Header>
+        <div className="product__header__buttons">
+          <button className="product__header__buttons__button">
+            Duplicate
+          </button>
+          <ButtonPrimary>Create</ButtonPrimary>
+        </div>
+      </Header>
+      <div className="product__container">
+        <div className="details__container__content">
+          <div className="details__container__content__details">
+            <div className="details__container__content__details__entry">
+              <div className="details__container__content__details__entry__subtitle">
+                Product ID
+              </div>
+              <div className="details__container__content__details__entry__title">
+                23900
+              </div>
+            </div>
+            <div className="details__container__content__details__entry">
+              <div className="details__container__content__details__entry__subtitle">
+                Name
+              </div>
+              <div className="details__container__content__details__entry__title">
+                Panadol 500mg
+              </div>
+            </div>
+            <div className="details__container__content__details__entry">
+              <div className="details__container__content__details__entry__subtitle">
+                Product ID
+              </div>
+              <div className="details__container__content__details__entry__title">
+                23900
+              </div>
+            </div>
+          </div>
+          <div className="details__container__content__batch"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Details;
