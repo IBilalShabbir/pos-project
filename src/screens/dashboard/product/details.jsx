be54f782287568fa1bfd87767ce63851ec92a9ca
Asim Rasheed <asimwarraich533@gmail.com>
2023-02-18 12:49:00 +0500

    product details done

--- src/screens/dashboard/product/details.jsx
+++ src/screens/dashboard/product/details.jsx
@@ -1,6 +1,15 @@
 import { ButtonPrimary, Header } from "../../../components";
 
 function Details() {
+  const BatchCard = [
+    "5 March 2022",
+    "500 left",
+    "500 left",
+    "500 left",
+    "500 left",
+    "500 left",
+    "500 left",
+  ];
   return (
     <>
       <Header>
@@ -79,10 +88,58 @@ function Details() {
               </div>
             </div>
           </div>
-          <div className="details__container__content__batch"></div>
+          <div className="details__container__content__batch__main">
+            <ProductDetail heading="Batch 3-Match-22" value={BatchCard} />
+            <ProductDetail heading="Batch 3-Match-22" value={BatchCard} />
+            <ProductDetail heading="Batch 3-Match-22" value={BatchCard} />
+          </div>
         </div>
       </div>
     </>
+  );
+}
+
+function ProductDetail({ heading, value }) {
+  return (
+    <div className="details__container__content__batch__wraper">
+      <div className="details__container__content__batch__wraper__heading">
+        {heading}
+      </div>
+      <div className="details__container__content__entry__wraper">
+        <div className="details__container__content__entry__haading">
+          <div className="details__container__content__entry__haading__entry">
+            Expiry Date
+          </div>
+          <div className="details__container__content__entry__haading__entry">
+            Quantity
+          </div>
+          <div className="details__container__content__entry__haading__entry">
+            P.Price
+          </div>
+          <div className="details__container__content__entry__haading__entry">
+            Sale Price
+          </div>
+          <div className="details__container__content__entry__haading__entry">
+            Dis.Price
+          </div>
+          <div className="details__container__content__entry__haading__entry">
+            Distributor
+          </div>
+          <div className="details__container__content__entry__haading__entry">
+            Note
+          </div>
+        </div>
+        <div className="details__container__content__entry__haading">
+          {value.map((item) => {
+            return (
+              <div className="details__container__content__entry__subhaading__entry">
+                {item}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
   );
 }
 
